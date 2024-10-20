f61ba31f1663563af35d2e6461ce759133238b74
rana Aditya <ranarajput9548@gmail.com>
2024-10-20 18:45:20 +0530

    inventory service created

---
+++ src/main/java/com/microservice/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,11 @@
+package com.microservice.inventoryservice.repository;
+
+
+
+import com.microservice.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, Integer quantity);
+}
+
---
+++ src/main/resources/db/migration/V2__and_inventory.sql
@@ -0,0 +1,4 @@
+insert into t_inventory(quantity, sku_code)
+values (100, 'iphone-15'),
+       (100, 'pixel-8'),
+       (100, 'galaxy-s24');
